c9890fbe945c4ccb0d37852517d205ed608cbaf9
Fernando <barreramelchorf@gmailcom>
2019-09-07 17:55:59 -0500

    primer commit

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {'mensaje': 'la cagas morro'}
 
 api.add_resource(HelloWorld, '/')
 
